47baa36c3f24283c65274fb6da06379ef4aaa91e
Nathan Keith <nashvegasnate@gmail.com>
2021-04-20 17:55:51 -0500

    fixed hover

--- src/App/index.js
+++ src/App/index.js
@@ -1,36 +1,69 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import {
+  Button,
+  Popover,
+  PopoverHeader,
+  PopoverBody
+} from 'reactstrap';
 import './App.scss';
+import getQuestions from '../helpers/data/avatarData';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  const [domWriting, setDomWriting] = useState('Nothing Here!');
+  const [allQuestions, setAllQuestions] = useState([]);
+  const [singleQuestion, setSingleQuestion] = useState({});
+  const [showAnswer, setShowAnswer] = useState(false);
+  const [popoverOpen, setPopoverOpen] = useState(false);
 
-  const handleClick = (e) => {
-    console.warn(`You clicked ${e.target.id}`);
-    setDomWriting(`You clicked ${e.target.id}! Check the Console!`);
+  const toggle = () => setPopoverOpen(!popoverOpen);
+
+  const handleClick = () => {
+    if (showAnswer) {
+      setShowAnswer(false);
+      setSingleQuestion(allQuestions[Math.floor(Math.random() * allQuestions.length)]);
+    } else {
+      setShowAnswer(true);
+    }
   };
+
+  useEffect(() => {
+    getQuestions()
+      .then((questions) => {
+        setAllQuestions(questions);
+        setSingleQuestion(questions[Math.floor(Math.random() * questions.length)]);
+      });
+  }, []);
 
   return (
     <div className='App'>
-      <h2>INSIDE APP COMPONENT</h2>
-      <div>
-        <button
-          id='this-button'
-          className='btn btn-info'
-          onClick={handleClick}
+      <h1>{singleQuestion.question}</h1>
+      <p>{showAnswer && singleQuestion.correctAnswer}</p>
+      {showAnswer ? ''
+        : <div>
+      <Button id="Popover1" type="button">
+        Launch Popover
+      </Button>
+      <Popover
+        placement="bottom"
+        isOpen={popoverOpen}
+        target="Popover1"
+        toggle={toggle}
+        trigger="hover"
         >
-          I am THIS button
-        </button>
+        <PopoverHeader>Possible Answers</PopoverHeader>
+        <PopoverBody>
+          <ul>
+            {singleQuestion.possibleAnsers?.map((pa, i) => <li key={i}>{pa}</li>)}
+        </ul>
+        </PopoverBody>
+      </Popover>
       </div>
-      <div>
-        <button
-          id='that-button'
-          className='btn btn-primary mt-3'
-          onClick={handleClick}
-        >
-          I am THAT button
-        </button>
-      </div>
-      <h3>{domWriting}</h3>
+      }
+      {popoverOpen ? ''
+        : <Button color="info" onClick={handleClick}>
+        {showAnswer ? 'Get Another Question' : 'Get Answer'}
+      </Button>
+      }
     </div>
   );
 }
